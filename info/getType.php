b238fa3ba601e1c8cd583759ad130f24bdfb6b9c
mipiip <dreamin93.mh61334@gmail.com>
2023-10-16 20:58:04 +0900

    first commit

---
+++ info/getType.php
@@ -0,0 +1,13 @@
+<?php
+
+$str = "文字列です";
+$int_number = 100;
+$arr = array(A, B, C, D);
+$bool = "true";
+$null = NULL;
+
+echo gettype($str) . PHP_EOL;
+echo gettype($int_number) . PHP_EOL;
+echo gettype($arr) . PHP_EOL;
+echo gettype($bool) . PHP_EOL;
+echo gettype($null) . PHP_EOL;
